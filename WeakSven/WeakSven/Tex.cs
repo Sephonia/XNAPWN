7015bd1ee91d4dafd0d6990d3d19c1ee55a45ed4
Monica <dragonswings004@yahoo.com>
2014-09-13 11:37:31 -0700

    Doin it again

---
+++ WeakSven/WeakSven/Tex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WeakSven
+{
+    class Tex
+    {
+        private Texture2D img = null;
+        public Rectangle Rect { get; private set; }
+
+        public Tex(Texture2D texture, int x, int y)
+        {
+            img = texture;
+            Rect = new Rectangle(x, y, 25, 25);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(img, Rect, Color.White);
+        }
+    }
+}
