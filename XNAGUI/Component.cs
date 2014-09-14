936c902390a9fc3270e96f24c810aa8bfb056b24
Monica <dragonswings004@yahoo.com>
2014-09-13 17:08:53 -0700

    LAAAAAAAAAADYBUUUUUUUUUUG

---
+++ XNAGUI/Component.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace XNAGUI
+{
+    class Component
+    {
+        private Texture2D square = null;
+
+        public Vector2 Position { get; set; }
+        public Rectangle Rect { get; set; }
+
+        private bool hovering = false;
+        private bool clicked = false;
+
+        public delegate void ComponentEvent(Component sender);
+
+        public event ComponentEvent onMouseOver = null;
+        public event ComponentEvent onMouseOut = null;
+        public event ComponentEvent onMouseDown = null;
+        public event ComponentEvent onMouseUp = null;
+        public event ComponentEvent onClick = null;
+
+        public Component(Rectangle whereItsAt) 
+        {
+            Rect = whereItsAt;
+        }
+
+        public void Update()
+        {
+            MouseState mouse = Mouse.GetState();
+
+            if(Rect.Contains(mouse.X, mouse.Y))
+            {
+                if(!hovering)
+                {
+                    if(onMouseOver != null)
+                        onMouseOver(this);
+
+                    if (mouse.LeftButton == ButtonState.Pressed)
+                    {
+                        if(!clicked)
+                            if(onMouseDown != null)
+                                onMouseDown(this);
+
+                        clicked = false;
+                    }
+                    else if(mouse.LeftButton == 
+
+                    }
+
+                    hovering = true;
+                }
+
+                
+            }
+        }
+    }
+}
---
+++ XNAGUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XNAGUI")]
+[assembly: AssemblyProduct("XNAGUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("EDMC")]
+[assembly: AssemblyCopyright("Copyright © EDMC 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("6ae0002d-e0de-4603-bd11-5a7157e06e9d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
