c0d5bc79c2429d30dd7fa23e71bc807f1ead9f7b
Sephonia <richardbarrettdev@gmail.com>
2014-09-27 17:06:17 -0700

    Fixed

---
+++ WeakSven/WeakSven/BGMSFX.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+
+namespace WeakSven
+{
+    class BGMSFX
+    {
+        public Song Song { get; set; }
+        public double startTime = 0.0;
+
+        public bool Play(GameTime gameTime)
+        {
+            if (startTime == 0 ||
+                gameTime.TotalGameTime.TotalSeconds - startTime >= Song.Duration.TotalSeconds)
+            {
+                MediaPlayer.Play(Song);
+                startTime = gameTime.TotalGameTime.TotalSeconds;
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
